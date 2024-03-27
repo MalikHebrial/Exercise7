9550c07973160deaeb0394c1e0b5699db4560a70
MalikHebrial <164378627+MalikHebrial@users.noreply.github.com>
2024-03-27 22:41:00 +0500

    45 quations solve

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//additio
+const additionResult = 4 + 4 
+
+console.log("additionResult:" + additionResult)
+
+//Subtraction
+
+const Subtraction = 16-8
+console.log("Subtraction Result: "+ Subtraction) 
+
+// Multiplaction
+
+const Multiplaction = 4 * 2;
+console.log("Multiplaction Result: "+ Multiplaction)
+
+
+
